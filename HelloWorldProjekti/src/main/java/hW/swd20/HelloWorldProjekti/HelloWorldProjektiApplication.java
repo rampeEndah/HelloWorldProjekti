11e38ff28b2762010632f0181ba7ef69d5663813
Endah Rahmannudin <a1702578@myy.haaga-helia.fi>
2019-09-02 10:28:26 +0300

    ekat lisäykset

---
+++ HelloWorldProjekti/src/main/java/hW/swd20/HelloWorldProjekti/HelloWorldProjektiApplication.java
@@ -0,0 +1,13 @@
+package hW.swd20.HelloWorldProjekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldProjektiApplication.class, args);
+	}
+
+}
